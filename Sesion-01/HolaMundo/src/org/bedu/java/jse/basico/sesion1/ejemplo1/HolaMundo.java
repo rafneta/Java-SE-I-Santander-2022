dce5378ce9a3a6cd258489885eb69a09f04519ff
Rafa mac pro 22 <raf_neta@hotmail.com>
2023-01-10 20:37:44 -0600

    ejemplo1

---
+++ Sesion-01/HolaMundo/src/org/bedu/java/jse/basico/sesion1/ejemplo1/HolaMundo.java
@@ -0,0 +1,8 @@
+package org.bedu.java.jse.basico.sesion1.ejemplo1;
+
+
+public class HolaMundo {
+    public static void main(String[] args){
+        System.out.println("¡¡Hola Mundo!!");
+    }
+}
