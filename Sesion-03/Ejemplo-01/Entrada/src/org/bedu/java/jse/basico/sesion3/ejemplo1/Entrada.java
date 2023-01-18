a3f62f11ff40a48dcf63864889762f2f70a92d40
rafneta <raf_neta@hotmail.com>
2023-01-18 17:25:33 -0600

    sesion3 ejemplo1

---
+++ Sesion-03/Ejemplo-01/Entrada/src/org/bedu/java/jse/basico/sesion3/ejemplo1/Entrada.java
@@ -0,0 +1,22 @@
+package org.bedu.java.jse.basico.sesion3.ejemplo1;
+
+import java.io.Console;
+import java.util.Scanner;
+
+public class Entrada {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Escribe tu nombre: ");
+        String nombre = scanner.nextLine();
+        System.out.println("Hola " + nombre);
+        Console console = System.console();
+
+        if (console == null){
+            System.err.println("No hay consola.");
+            System.exit(1);
+        }
+
+        nombre = console.readLine("%s", "Escribe tu nombre: ");
+        System.out.println("Hola " + nombre);
+    }
+}
