ab9589bb80a769a9215b547c4324be0c7beb94cd
Rafa mac pro 22 <raf_neta@hotmail.com>
2023-02-07 07:37:51 -0600

    sesion8 ejemplo 1

---
+++ Sesion-08/Ejemplo-01/Archivo/src/org/bedu/java/jse/basico/sesion8/ejemplo1/Archivo.java
@@ -0,0 +1,24 @@
+package org.bedu.java.jse.basico.sesion8.ejemplo1;
+
+import java.io.File;
+
+public class Archivo {
+    public static void main(String[] args) {
+        File archivo = new File("/Users/rafamtz/Library/CloudStorage/GoogleDrive-ramartinezr@tec.mx/Mi unidad/Personal/BecasSantander/BeduJavaWeb/Fase1/gitfase1/Sesion-08/temp/cursos/cursos.txt");
+
+        System.out.println("Nombre del archivo:" + archivo.getName());
+        System.out.println("Ruta: " + archivo.getPath());
+        System.out.println("Ruta relativa: " + archivo.getAbsolutePath());
+        System.out.println("Padre: " + archivo.getParent());
+        System.out.println("Existe: " + archivo.exists());
+        System.out.println("Está oculto: " + archivo.isHidden());
+
+        if (archivo.exists()) {
+            System.out.println("Se puede escribir en él: " + archivo.canWrite());
+            System.out.println("Se puede leer: " + archivo.canRead());
+            System.out.println("Se puede ejecutar: " + archivo.canExecute());
+            System.out.println("Es un directorio: " + archivo.isDirectory());
+            System.out.println("Tamaño del archivo en bytes: " + archivo.length());
+        }
+    }
+}
